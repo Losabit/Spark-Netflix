480279de659fcdab596ff197a28951549e0a7711
PaulBenabou <paulbenabou@gmail.com>
2021-01-21 12:32:39 +0100

    Added main load CSV + gitignore

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,5 +2,19 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object main extends App {
-  print("test")
+  // Defining the spark Session
+  val spark = SparkSession
+    .builder()
+    .appName("SparkNetflix")
+    .master("local[*]")
+    .getOrCreate()
+
+  // Setting log level to ERROR to Avoid multiple INFO logs
+  spark.sparkContext.setLogLevel("ERROR")
+
+  // Reading the DataFrame from source File
+  val stackDF = spark.read.option("inferSchema", "true").option("header", "true").csv("resources/netflix_titles.csv")
+  println(stackDF.toString())
+
+  // TODO :)
 }
