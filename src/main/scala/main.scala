f2e08051a8dfbb0fdfbb4fe3d190ccb924e46364
Marvin <44065639+Mrenoir@users.noreply.github.com>
2021-02-12 01:01:06 +0100

    Affichage

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,21 +8,34 @@ object main extends App {
     .appName("SparkNetflix")
     .master("local[*]")
     .getOrCreate()
-
   // Setting log level to ERROR to Avoid multiple INFO logs
   spark.sparkContext.setLogLevel("ERROR")
-
   // Reading the DataFrame from source File
   val netflixDF = spark.read.option("inferSchema", "true").option("header", "true").csv("resources/netflix_titles.csv")
-  println(netflixDF.printSchema())
-
   val dataset :  Dataset[(String,String)] = Utils.fieldToList(netflixDF, 5)
   //.withColumnRenamed("_1", "id").withColumnRenamed("_2", "director")
+
+  // Le plus gros pays producteur de serie ou film netflix
+  println("Le pays le plus représenté" )
   println(mostElem(dataset, "country"))
-  getMostRatedType(netflixDF)
-/*
+  println("---------------------------------------")
+  // Le type le mieux noté
+  println()
+  println("Le type  le mieux noté  : ")
+  println(getMostRatedType(netflixDF))
+
+  println("---------------------------------------")
+  val datasetD :  Dataset[(String,String)] = Utils.fieldToList(netflixDF, 3)
+  println("Directeur le plus présent sur Netflix : ")
+  // Le directeur le plus représenté
+  println(mostElem(datasetD, "director"))
+
+  println("---------------------------------------")
+
+  //  Temps moyens
+  println("Temps moyen d'un show   : ")
   println(NetflixData.averageShowDuration(NetflixData.showTypes(1),netflixDF))
-  println(mostDirector(netflixDF))
+  println("---------------------------------------")
   println(NetflixData.typeIteration(netflixDF))
-*/
+
 }
